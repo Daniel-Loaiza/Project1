8664947c2afe2957507bf7b9abbed22a7c7cd489
Daniel-Loaiza <danielloaizaisi@gmail.com>
2023-01-20 12:59:19 -0500

    Added first behaviour

--- namer3/lib/main.dart
+++ namer3/lib/main.dart
@@ -27,6 +27,12 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -40,6 +46,14 @@ class MyHomePage extends StatelessWidget {
           //Text('A random idea:'),
           Text('A random AWESOME idea:'), // ← Example change.
           Text(appState.current.asLowerCase),
+          // ↓ Add this.
+          ElevatedButton(
+            onPressed: () {
+              // print('button pressed!');
+              appState.getNext(); // ← This instead of print().
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
